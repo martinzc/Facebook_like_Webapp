b9314d29d45eb313ca2bee573bc2a4cbb59aa4eb
Martin Zhou <martinzhouchao@gmail.com>
2016-11-15 23:54:27 -0600

    no message

--- inclass-24/auth.js
+++ inclass-24/auth.js
@@ -2,17 +2,19 @@
 
 var redis = require('redis').createClient('redis://h:pdgkn7cikjlp1i2437ot7o4kuq3@ec2-54-221-251-36.compute-1.amazonaws.com:10989')
 var sid = "session id"
-var userObj = {name: "test user", password: "password"}
 
 const register = (req, res) => {
 	res.send({ result: "success", username:"test user"})
 }
 
 const login = (req, res) => {
-	redis.hmset(sid, userObj)
-	redis.hgetall(sid, function(err, userObj) {
-		console.log(sid + ' mapped to ' + userObj)
-	})
+	console.log(isLoggedIn(req))
+	if (isLoggedIn(req)) {
+		console.log("user is logged in already")
+	} else {
+		var payload = {name: req.body.username}
+		redis.hmset(sid, {"name": req.body.username})
+	}
 	res.send({ username: "test user", result: "success"})
 }
 
@@ -22,6 +24,16 @@ const logout = (req, res) => {
 
 const changePw = (req, res) => {
 	res.send({ username: "test user", status: "will not change"})
+}
+
+function isLoggedIn(req) {
+	let bool = false
+	redis.hgetall(sid, function(err, userObj) {
+		if (userObj.name == req.body.username) {
+			bool = true
+		}
+	})
+	return false
 }
 
 
